29420714340a55f9424ded228c8c8fe967e056d8
nerdk312 <nawid.keshtmand@hotmail.com>
2025-02-24 10:57:21 +0000

    updated workout schedule

--- training_schedule_171224.py
+++ training_schedule_171224.py
@@ -127,14 +127,14 @@ weekly_exercises = {'Week 1':{'Monday':['Front Sled','Tricep Pushdown','Barbell 
 
                             'Friday':['Side Sled','Tricep Overhead','Peterson Iso Step Up','Two Leg Iso Lying Hamstring Curl','Iso Single Leg Monkey Foot','Iso Lat Pull Down Machine','Iso Pistol Squat','Iso 90-90 Hip Push Press','Iso Cable External Rotation','One Arm Hang','Single Lateral Jump'],
 
-                            'Saturday':['Front Sled','Tricep Pushdown','Two Legged Hip Thrust(s)','Side Farmer Walk(Double)','Trap 3 Raise(s)','Light Two Arm Hang','Depth Drop','Max Incline Side Sprinting'],
+                            'Saturday':['Front Sled','Tricep Pushdown','Two Legged Hip Thrust(s)','Cable Hammer Curl(s)','Side Farmer Walk(Double)','Trap 3 Raise(s)','Light Two Arm Hang','Depth Drop','Max Incline Side Sprinting'],
 
                             'Sunday':['Tricep Overhead','Iso Two Legged Leg Press','Iso Reverse Curl','Iso Wrist Rotation','One Leg Iso Seated Hamstring Curl','Iso Barbell Jefferson Curl', 'Iso Tricep Extension','Iso Back Extension','Iso Chin Ups','Iso Dragon Flag','Iso Trapbar Shrug','Iso Rear Delt Fly']
                             },
 
 
 
-                        'Week 7':{'Monday':['Front Sled','Tricep Pushdown','Squat Clean(p)','Seated Dumbbell Press(s)','Barbell Deficit Deadlift(s)', 'Banded Neck Sides(p)','Single Leg Standing Calf Raise(s)', 'Max Two Arm Hang','Sitting Box Jump'],
+                        'Week 7':{'Monday':['Front Sled','Tricep Pushdown','Squat Clean(p)','Barbell Deficit Deadlift(s)', 'Banded Neck Sides(p)','Single Leg Standing Calf Raise(s)', 'Max Two Arm Hang','Sitting Box Jump'],
 
                             'Tuesday':['Side Sled','Tricep Overhead','Iso Hack Squat','Two Leg Iso Nordic Hamstring Curl','Iso Barbell Military Press','Iso Incline Dumbbell Press','Iso Dumbbell Sumo Deadlift', 'Iso Banded neck rotiation','Iso Single Leg Seated Calf Raise', 'One Arm Hang','Max Incline Backwards Sprinting'],
 
@@ -150,7 +150,7 @@ weekly_exercises = {'Week 1':{'Monday':['Front Sled','Tricep Pushdown','Barbell 
                                       'Iso Back Extension One Leg','Iso Barbell Preacher Curl','Iso Ab Machine','Iso Trapbar Shrug','Iso Row Machine']
                             },
 
-                        'Week 8':{'Monday':['Front Sled','Tricep Pushdown','Reverse Nordic(s)','Landmine Split Jerk(p)','Incline Barbell Press(s)','Barbell Deadlift(s)', 'Banded Neck Sides(s)', 'Max Two Arm Hang','Depth Jump'],
+                        'Week 8':{'Monday':['Front Sled','Tricep Pushdown','Landmine Split Jerk(p)','Incline Barbell Press(s)','Barbell Deadlift(s)', 'Banded Neck Sides(s)', 'Max Two Arm Hang','Depth Jump'],
 
                             'Tuesday':['Side Sled','Tricep Overhead','Peterson Step Up','One Leg Iso Nordic Hamstring Curl','Iso Barbell Back Squat','Iso Barbell Military Press','Iso Dumbbell Bench Press','Iso Dumbbell Romanian Deadlift','Iso Dumbbell Wrist Flexion','Iso Dumbbell Wrist Curl','Iso One Leg Tibialis Raise', 'One Arm Hang','Max Speed Side Sprinting'],
 
@@ -164,6 +164,37 @@ weekly_exercises = {'Week 1':{'Monday':['Front Sled','Tricep Pushdown','Barbell 
 
                             'Sunday':['Tricep Overhead','Iso Dumbbell Cossack Squat','Iso Two legged Leg Extension','Iso Seated Good Morning', 'Iso Dips','Iso Cable Hip Adduction','Iso Cable Hip Abduction','Iso Neck Upwards','Iso Neck Downwards',
                                       'Iso One Leg Hip Thrust','Iso Incline Dumbbell Hammer Curl','Iso QL extension','Iso Frontal Raise','Iso Face Pulls'],
+
+                            },
+                            # Deload week
+                         'Week Deload1':{'Monday':['Front Sled','Tricep Pushdown','Reverse Nordic(s)','Seated Dumbbell Press(s)','Cable Chest Fly(s)', 'Max Two Arm Hang'],
+
+                            'Tuesday':['Side Sled','Tricep Overhead','Iso Hack Squat','Iso Cable Rear Flys','One Arm Hang'],
+
+                            'Wednesday':['Front Sled','Tricep Pushdown','Cable Lateral Raise(s)','Landmine Split Squat(s)','Medium Two Arm Hang'],
+
+                            'Thursday':['Front Sled','Tricep Pushdown','Standing Good Morning(s)','Front Barbell Horsestance Squat(s)','Heavy Two Arm Hang'],
+
+                            'Friday':['Side Sled','Tricep Overhead','Iso Bulgarian Split Squat(s)','Iso Cable Frontal Raise','One Arm Hang'],
+
+                            'Saturday':['Front Sled','Tricep Pushdown','Cable Reverse Curl(s)','Cable Frontal Raise(s)','Two Legged Nordic Hamstring Curl(s)','Light Two Arm Hang'],
+
+                            'Sunday':['Side Sled','Tricep Overhead','Iso Cable Row','Iso Back Barbell Horsestance Squat','Iso Cable Bicep Curl','Iso Two Legged Back Extension'],
+
+                            },
+                        # Deload week
+                        'Week Deload2':{'Monday':['Front Sled','Tricep Pushdown','Hack Squat(s)','Cable Rear Fly(s)' 'Max Two Arm Hang','Max Two Arm Hang'],
+
+                            'Tuesday':['Side Sled','Tricep Overhead','Iso Reverse Nordic','Iso Seated Dumbbell Press','Iso Cable Chest Fly','One Arm Hang'],
+
+                            'Wednesday':['Front Sled','Tricep Pushdown','Cable Frontal Raise(s)','Bulgarian Split Squat(s)'],
+
+                            'Thursday':['Front Sled','Tricep Pushdown','Cable Row(s)','Back Barbell Horsestance Squat(s)','Heavy Two Arm Hang','Max Incline Backwards Sprinting'],
+
+                            'Friday':['Side Sled','Tricep Overhead','Iso Cable Lateral Raise','Iso Landmine Split Squat(s)'],
+
+                            'Saturday':['Front Sled','Tricep Pushdown','Cable Bicep Curl(s)','Two Legged Back Extension(s)','Light Two Arm Hang'],
+                            'Sunday':['Iso Standing Good Morning','Iso Front Barbell Horsestance Squat(s)','Iso Cable Reverse Curl(s)','Iso Cable Frontal Raise','Two Leg Iso Nordic Hamstring Curl'],
 
                             },
 
@@ -182,8 +213,8 @@ with open('workout_log.txt', 'r') as file:
 content = content.lower() # Remove capitalization
 
 day = 'Sunday'
-week = 'Week 3'
-start_date = '2024-12-30'
+week = 'Week 1'
+start_date = '2024-02-17'
 weekly=True
 '''
 start_date = datetime.date.fromisoformat('2019-12-04')
@@ -299,4 +330,14 @@ Breakfast Middle splits, Pistol Squat
 Lunch Couch Stretch, L-sit
 Dinner Side Tricep stretch, Ankle inversions
 
+'''
+
+'''
+import pickle
+
+# Opening and loading a Pickle file
+with open('my_dict.pkl', 'rb') as f:
+    loaded_dict = pickle.load(f)
+
+print(loaded_dict)
 '''
